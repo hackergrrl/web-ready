b59e20b67eb800e959bcfc43f563f4e9dde8ae58
noffle <sww@eight45.net>
2019-10-23 10:04:24 -0700

    add title tag

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 
 var http = require('http')
 
+var html = `
+  <title>web-ready</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <form method="POST">
+    <button style="width:200px;height:100px;" type="submit">ready</button>
+  </form>
+`
+
 http.createServer((req, res) => {
   if (req.method === 'GET') {
     res.setHeader('Content-Type', 'text/html')
-    res.write('<meta name="viewport" content="width=device-width,initial-scale=1">')
-    res.end('<form method="POST"><button style="width:200px;height:100px;" type="submit">ready</button></form>')
+    res.end(html)
   } else {
     process.exit(0)
   }
