007d9bbdc974dd58df6b12702805f5a141367836
noffle <sww@eight45.net>
2019-10-22 18:34:07 -0700

    impl

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http')
+
+http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<meta name="viewport" content="width=device-width,initial-scale=1">')
+    res.end('<form method="POST"><button style="width:200px;height:100px;" type="submit">ready</button></form>')
+  } else {
+    process.exit(0)
+  }
+}).listen(5000, () => console.log('listening on port 5000'))
+
